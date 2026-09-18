161be14d04aab6ce287c78b76df08b0bab83c25e
martin <50048880@est.ort.edu.ar>
2026-09-18 14:28:33 -0300

    lpqlrmp

--- Navegación/localizacion.js
+++ Navegación/localizacion.js
@@ -10,8 +10,8 @@ const getmarcador = async (req, res) => {
     // Completar con la consulta que devuelve las canciones de un album
     // Recordar que los parámetros de una consulta GET se encuentran en req.params
     // Deberían devolver los datos de la misma forma que getCanciones
-    const result = await query("SELECT canciones.id, canciones.nombre, canciones.duracion, canciones.reproducciones, albumes.nombre as nombre_album, artistas.nombre as nombre_artista from canciones join albumes on albumes.id = canciones.album join artistas on artistas.id = albumes.artista where album = $1  ", [req.params.id]);
-    res.send(result.rows);
+    const result = await query("SELECT m.id AS marcador_id, m.ancho_cm, m.alto_cm, m.pared, n.id AS nodo_id, n.nombre AS nombre_lugar, n.posicion_x, n.posicion_y FROM marcadores m JOIN nodos n ON m.nodo_id = n.id WHERE m.id = $1", [req.params.id]);
+    res.json(result.rows);
   };
 
   const marcadores = {
