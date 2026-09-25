785a422f2825dfd2e6bcc1429c31ad233ba246d8
Martincho250 <50048880@est.ort.edu.ar>
2026-09-25 14:12:44 -0300

    se guarda en json

--- comunicacion/localizacion.js
+++ comunicacion/localizacion.js
@@ -1,5 +1,6 @@
 // Node.js (Express)
 import { query } from "./db.js";
+import fs from "fs";
 
 /**
  * Devuelve un artista por su ID.
@@ -11,7 +12,10 @@ import { query } from "./db.js";
         "SELECT m.id AS marcador_id, m.ancho_cm, m.alto_cm, m.pared, n.id AS nodo_id, n.numero AS numero_lugar, n.nombre AS nombre_lugar, n.posicion_x, n.posicion_y FROM marcadores m JOIN nodos n ON m.nodo_id = n.id WHERE m.id = $1",
         [req.params.id]
     );
-    res.json(result.rows[0]);
+    const datatosave = {resultado: result.rows[0]};
+    fs.writeFileSync("resultado.json", JSON.stringify(datatosave, null, 2), "utf-8"); 
+
+    res.json(datatosave);
   }
 
   const marcadores = {
